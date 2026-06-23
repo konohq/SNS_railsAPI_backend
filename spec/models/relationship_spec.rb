4c7b04324dc61a6351243bcc494361c3961d7d06
kono <aporokun114514@gmail.com>
2026-06-23 18:44:45 +0900

    chore: RSpec中心にテスト環境とCIを整理

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:follower) { FactoryBot.create(:user) }
+  let(:followed) { FactoryBot.create(:user) }
+
+  it "異なるユーザー同士であれば有効である" do
+    relationship = described_class.new(follower: follower, followed: followed)
+
+    expect(relationship).to be_valid
+  end
+
+  it "自分自身をフォローする関係は無効である" do
+    relationship = described_class.new(follower: follower, followed: follower)
+
+    expect(relationship).not_to be_valid
+    expect(relationship.errors[:followed]).to be_present
+  end
+
+  it "同じ組み合わせのフォロー関係は重複できない" do
+    described_class.create!(follower: follower, followed: followed)
+    duplicate_relationship = described_class.new(follower: follower, followed: followed)
+
+    expect(duplicate_relationship).not_to be_valid
+    expect(duplicate_relationship.errors[:follower_id]).to be_present
+  end
 end
